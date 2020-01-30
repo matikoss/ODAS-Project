2a7a2c52c1f90a71cd00f0683de152c6359652be
matikoss <m.kossakowski0@gmail.com>
2020-01-30 21:11:44 +0100

    Dodano pole quality przy entropii

--- odas-app/app/static/js/user.js
+++ odas-app/app/static/js/user.js
@@ -7,6 +7,22 @@ const calculatePasswordEntropy = (password) => {
 const changeEntropyOutput = (entropy) => {
     const entropyText = $("#new-password-entropy-result");
     entropyText.text(`Password entropy: ${entropy}`);
-    console.log(entropy);
-    console.log(entropyText)
+    const qualityText = $("#new-pass-quality-field");
+    qualityText.text(`Strength: ${calculateQuality(entropy)}`);
+};
+
+const calculateQuality = (entropy) => {
+    let result;
+    if (entropy < 28) {
+        result = "Very Weak"
+    } else if (entropy >= 28 && entropy <= 35) {
+        result = "Weak"
+    } else if (entropy >= 35 && entropy <= 59) {
+        result = "Reasonable"
+    } else if (entropy >= 60 && entropy <= 127) {
+        result = "Strong"
+    } else if (entropy >= 128) {
+        result = "Very Strong"
+    }
+    return result
 };
